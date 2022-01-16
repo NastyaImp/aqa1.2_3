e4081952d0633e6f348d1807ebb87d64e2cbf376
nastyaImp <imperiian@yandex.ru>
2022-01-16 16:56:08 +0300

    test failed

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test success") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
